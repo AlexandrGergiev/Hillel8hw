4f07da097a32b5c5128589a439910421aa248477
AlexandrGergiev <gergieverty3@gmail.com>
2022-02-04 23:29:18 +0200

    1upd

---
+++ catalog/urls.py
@@ -0,0 +1,22 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("books", views.BookListView.as_view(), name="books"),
+    path("book/<int:pk>", views.BookDetailView.as_view(), name="book"),
+    path("authors", views.AuthorListView.as_view(), name="authors"),
+    path("author/<int:pk>", views.AuthorDetailView.as_view(), name="author"),
+    path(
+        "book/instance/<uuid:pk>/reserve",
+        views.reserve_book_form,
+        name="reserve-book-form",
+    ),
+    path("author/create", views.AuthorCreateView.as_view(), name="author-create"),
+    path(
+        "author/update/<int:pk>", views.AuthorUpdateView.as_view(), name="author-update"
+    ),
+    path(
+        "author/delete/<int:pk>", views.AuthorDeleteView.as_view(), name="author-delete"
+    ),
+]
---
+++ catalog/views.py
@@ -0,0 +1,114 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from .models import Book, Author, BookInstance, Genre
+from .forms import ReserveBookForm
+from typing import Any
+
+import datetime
+
+
+# Create your views here.
+
+
+class AuthorCreateView(generic.CreateView):
+    model = Author
+    fields = "__all__"
+    initial = {
+        "date_of_death": "11/03/2009",
+    }
+
+
+class AuthorUpdateView(generic.UpdateView):
+    model = Author
+    fields = "__all__"
+    initial = {
+        "date_of_death": "11/03/2009",
+    }
+
+
+class AuthorDeleteView(generic.DeleteView):
+    model = Author
+    fields = "__all__"
+    initial = {
+        "date_of_death": "11/03/2009",
+    }
+
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = "book_list"
+    queryset = Book.objects.all()
+    template_name = "book/list.html"
+
+    # def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+    #     return super().get_context_data(**kwargs) | {"book_list": Book.objects.all()}
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = "book/detail.html"
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    context_object_name = "author_list"
+    queryset = Author.objects.all()
+    template_name = "author/list.html"
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    template_name = "author/detail.html"
+
+
+VISITS_KEY = "visits"
+
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact="a").count()
+    num_authors = Author.objects.all().count()
+
+    num_visits = request.session.get(VISITS_KEY, 0)
+    request.session[VISITS_KEY] = num_visits + 1
+
+    return render(
+        request,
+        "index.html",
+        context={
+            "num_books": num_books,
+            "num_instances": num_instances,
+            "num_instances_available": num_instances_available,
+            "num_authors": num_authors,
+            "num_visits": num_visits,
+        },
+    )
+
+
+def reserve_book_form(request, pk):
+    book_instance = get_object_or_404(BookInstance, pk=pk)
+
+    if request.method == "POST":
+        form = ReserveBookForm(request.POST)
+
+        if form.is_valid():
+            # Perform action
+            book_instance.status = "r"
+            book_instance.due_back = form.cleaned_data["return_date"]
+
+            book_instance.save()
+
+            # Redirect to success url
+            return HttpResponseRedirect("/")
+
+    else:
+        proposed_return_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = ReserveBookForm(initial={"return_date": proposed_return_date})
+
+    return render(
+        request,
+        "book/reserve.html",
+        {"form": form, "book_instance": book_instance},
+    )
